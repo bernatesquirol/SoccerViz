08ab437ebfc7413ada8dc99fe69e5a7c332d5266
AGregorc <anze.gregorc44@gmail.com>
2019-11-25 10:31:10 +0100

    Events bugfix

--- last_minute.py
+++ last_minute.py
@@ -10,21 +10,27 @@ for competition in competitions:
     # last minutes events
     events = events[(events['matchPeriod']=='2H') & (events['eventSec']>(45-minutes)*60)]
 
+    last_time = -20.0
     # count last minutes goals
     lmg = 0
-    for tags in events['tags']:
+    for idx, event in events.iterrows():
+        tags = event['tags']
+        if event['eventSec'] < last_time + 10:
+            continue
         for tag in tags:
             if 101 in tag.values():
                 lmg += 1
+                last_time = event['eventSec']
                 break
             if 102 in tag.values():
                 lmg += 1
+                last_time = event['eventSec']
                 # print(tag)
                 break
 
     print(competition, lmg)
 
-# RESULTS: (Country, last minutes goals, all games)
+# RESULTS WRONG ??? : (Country, last minutes goals, all games)
 # England   168     380     0.442
 # France    156     380     0.411
 # Germany   130     306     0.424
@@ -34,3 +40,9 @@ for competition in competitions:
 # SO:
 # BEST: England
 # WORST: Spain
+
+# England   3
+# France    8
+# Germany   2
+# Spain     4
+# Italy     7
--- lead_changes.py
+++ lead_changes.py
@@ -7,9 +7,9 @@ competitions = ['England', 'France', 'Germany', 'Spain', 'Italy']
 def get_status(curr_dict):
     t_arr = list(curr_dict.values())
     if t_arr[0] > t_arr[1]:
-        return t_arr[0]
+        return 1
     elif t_arr[0] < t_arr[1]:
-        return t_arr[1]
+        return -1
     return 0
 
 
@@ -26,34 +26,51 @@ for competition in competitions:
         teams = mat_events['teamId'].unique()
         td = {teams[0]: 0, teams[1]: 0}
         current_status = 0  # 0 is draw
+        last_time = -20.0
+        sc = 0
 
         # first_half = mat_events[(mat_events['matchPeriod'] == '1H')]
-        for idx, tags in enumerate(mat_events['tags']):
+        for idx, event in mat_events.iterrows():
+            tags = event['tags']
+            if event['eventSec'] < last_time + 10:
+                continue
             for tag in tags:
                 if 101 in tag.values():
-                    td[mat_events.iloc[idx]['teamId']] += 1
+                    td[event['teamId']] += 1
+                    last_time = event['eventSec']
                     break
                 if 102 in tag.values():
-                    td[mat_events.iloc[idx]['teamId']] -= 1
+                    td[event['teamId']] -= 1
+                    last_time = event['eventSec']
                     # print(tag)
                     break
             if current_status != get_status(td):
                 status_changes += 1
+                sc += 1
             current_status = get_status(td)
 
         # second_half = mat_events[(mat_events['matchPeriod'] == '2H')]
-        # print(status_changes)
+        # print(matchId, td, sc)
 
     print(competition, status_changes, len(mat_id), status_changes/len(mat_id))
 
 
-# RESULTS: (Country, status changes, all games)
+# RESULTS WRONG!!!!: (Country, status changes, all games)
 # England   2012    380    5.29
 # France    2045    380    5.38
 # Germany   1691    306    5.52
 # Spain     2026    380    5.33
 # Italy     2006    380    5.27
 
+# RESULTS: (Country, status changes, all games, changes in avg. game)
+# England   523     380     1.37
+# France    570     380     1.5
+# Germany   456     306     1.49
+# Spain     519     380     1.36
+# Italy     549     380     1.44
+
 # SO:
-# BEST: Germany
-# WORST: Italy
+# BEST: France
+# WORST: Spain
+
+
