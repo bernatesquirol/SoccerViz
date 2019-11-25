7b8558010066d89646cc4926f8a682c9cfa59362
bernat <bernatesquirol@gmail.com>
2019-11-25 10:00:40 +0100

    client updated

--- client/src/MainScreen.js
+++ client/src/MainScreen.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Vega } from 'react-vega';
-import ClassificationMain from './plots/ClassificationMain';
+import VegaComponent from './components/VegaComponent';
+import {Container,Row,Col} from 'react-bootstrap'
 
-function App() {
-  console.log(spec,data)
-  return (
-    <div className="App">
-      <ClassificationMain></ClassificationMain>
-    </div>
-  );
-}
-
-export default App;
+export default ()=>(
+    <Container>
+        <Row>
+            <Col sm={4}><VegaComponent id='ClassificationMain'></VegaComponent></Col>
+            <Col sm={4}><VegaComponent id='MatchesMain'></VegaComponent></Col>
+            <Col sm={4}><VegaComponent id='PlayersMain'></VegaComponent></Col>
+        </Row>
+    </Container>
+)
