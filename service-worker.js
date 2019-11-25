acc2c2782276ab6e79bff9e5583b5d5cec641dda
bernat <bernatesquirol@gmail.com>
2019-11-25 15:22:53 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SoccerViz/precache-manifest.6472679511834759794ef8ea25c60a81.js"
+  "/SoccerViz/precache-manifest.6afccc6ec3b0f14e9cb09ffb14b7db7f.js"
 );
 
 self.addEventListener('message', (event) => {
